246637c8c69c837dd4305f8b5429397ac085f97a
ajith2001reddy <121050317+ajith2001reddy@users.noreply.github.com>
2026-02-25 00:21:24 -0500

    Update firebase.ts

--- frontend/lib/firebase.ts
+++ frontend/lib/firebase.ts
@@ -27,7 +27,7 @@ const firebaseConfig = {
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
+console.log("AUTH DOMAIN:", firebaseConfig.authDomain);
 // Prevent duplicate initialization
 const app =
     getApps().length === 0
