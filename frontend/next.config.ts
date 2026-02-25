4ac2548c241b1c18b779bc4761a46e6af3700cec
ajith2001reddy <121050317+ajith2001reddy@users.noreply.github.com>
2026-02-24 22:28:49 -0500

    Update next.config.ts
    as

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,25 @@
+// frontend/next.config.ts
 import type { NextConfig } from "next/dist/server/config-shared";
 
-
-const nextConfig = {
+const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "http://10.0.0.49:3000",
   ],
+
+  async headers() {
+    return [
+      {
+        // Apply to all routes
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
