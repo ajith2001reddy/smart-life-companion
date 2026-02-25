4fd5360da1473fac16fce1556e8c922ffecb23c1
ajith2001reddy <121050317+ajith2001reddy@users.noreply.github.com>
2026-02-24 23:17:47 -0500

    asa
    a

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,21 +5,6 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "http://10.0.0.49:3000",
   ],
-
-  async headers() {
-    return [
-      {
-        // Apply to all routes
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
