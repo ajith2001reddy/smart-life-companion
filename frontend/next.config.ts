df05675e448e889a4379b8d77391e7317d34c8c5
ajith2001reddy <121050317+ajith2001reddy@users.noreply.github.com>
2026-02-24 23:24:18 -0500

    feat: Initialize backend project with package.json and frontend Next.js configuration.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,7 @@
 import type { NextConfig } from "next/dist/server/config-shared";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    "http://10.0.0.49:3000",
-  ],
+
 };
 
 module.exports = nextConfig;
