d69edd684f60d8d7152d68a1410b00c0a54a8d5a
ajith2001reddy <121050317+ajith2001reddy@users.noreply.github.com>
2026-02-25 23:52:56 -0500

    Delete capacitor.config.ts

--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.smartlife.app',
-  appName: 'smartlife',
-  webDir: 'public'
-};
-
-export default config;
